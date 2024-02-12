f0e63c47036df23c1ff8e47ca0423d29580f9c9c
Zu33air <zu33air@gmail.com>
2024-02-12 14:02:57 +0500

    Seo 2

--- src/app/mobile-app/page.tsx
+++ src/app/mobile-app/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
   return (
     <div className="text-5xl font-extralight text-center py-10">
       Still in development process
+      <p className="font-bold text-center text-cyan-950"> How is it going</p>
     </div>
   );
 };
