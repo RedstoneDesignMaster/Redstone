4f5f00d26a86aa88651f8b87a46d4d476948cb16
Zu33air <zu33air@gmail.com>
2024-02-12 14:47:31 +0500

    Seo image

--- src/app/seo/Seosectionfour.tsx
+++ src/app/seo/Seosectionfour.tsx
@@ -17,7 +17,7 @@ const Seosectionfour = () => {
             Fliptainment
           </h5>
           <p className="text-wrap text-start line-clamp-4">
-            logo zeal proved to be a distinguished design agency. They helped us
+            Gojo zeal proved to be a distinguished design agency. They helped us
             in not just revamping our website with a whole new look and feel
             but, made it possible for us to achieve strong brand retention
             through their brilliant services and solutions.
