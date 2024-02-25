6886c912ba88b056dbb1391494116168e7a6f350
Zu33air <zu33air@gmail.com>
2024-02-26 03:46:07 +0500

    form integeration

--- src/app/components/Leadform.tsx
+++ src/app/components/Leadform.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Leadform = () => {
   return (
-    <form className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
+    <form
+      className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md"
+      action={"https://formspree.io/zu33air@gmail.com"}
+      method="POST"
+    >
       <h2 className="text-2xl font-semibold mb-4 text-center">Contact Us</h2>
       <div className="mb-4">
         <label
