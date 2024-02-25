8b407255a6ab9c46679e0ac597f48fd6567816bf
Zu33air <zu33air@gmail.com>
2024-02-26 03:28:11 +0500

    80% responsive done

--- src/app/components/Logosection.tsx
+++ src/app/components/Logosection.tsx
@@ -5,7 +5,7 @@ import Portfolioview from "./Portfolioview";
 const Logosection = () => {
   return (
     <div
-      className="grid grid-cols-2 sm:grid-cols-2 gap-4 justify-center px-4"
+      className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-center px-4"
       id="logo"
     >
       <Portfolioview source="1" path="Logos" width={300} height={300} />
