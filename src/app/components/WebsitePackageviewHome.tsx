76c05811ac3f4e65a11408bb088d4cdc5600be6f
RedstoneDesignMaster <redstonedesignmaster@gmail.com>
2024-12-05 13:46:04 +0500

    WebsitepackageviewFix

--- src/app/components/WebsitePackageviewHome.tsx
+++ src/app/components/WebsitePackageviewHome.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import packages from "../website/website packages/constantweb";
+import packages from "../website/website packages/pointersweb";
 import WebsitePackagez from "../website/website packages/WebsitePackagez";
 const WebsitePackageviewHome = () => {
   return (
     <div className="mt-20 py-10 px-14">
-      
       <div className="packages-container grid grid-cols-1 gap-y-8 sm:grid-cols-3 gap-x-8">
         {packages.map((pack, index) => (
           <WebsitePackagez key={index} pack={pack} />
--- src/app/website/website packages/WebsitePackageview.tsx
+++ src/app/website/website packages/WebsitePackageview.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import packages from "./constantweb";
+import packages from "./pointersweb";
 import WebsitePackagez from "./WebsitePackagez";
 
 const WebsitePackageview = (): any => {
