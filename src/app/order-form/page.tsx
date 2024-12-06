11bcccd1b2fe0f0afbbe72c6f5159ad0147ee81d
RedstoneDesignMaster <redstonedesignmaster@gmail.com>
2024-12-06 12:14:13 +0500

    major color changes

--- src/app/order-form/page.tsx
+++ src/app/order-form/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useState } from "react";
 import PopupModal from "./PopupModal";
 
-const page: React.FC = () => {
+const Page: React.FC = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
   return (
@@ -19,4 +19,4 @@ const page: React.FC = () => {
   );
 };
 
-export default page;
+export default Page;
